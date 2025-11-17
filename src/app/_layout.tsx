b61445600655bf1db5ddb2e7a1ec83f8eb80e4d5
Anxhelo Cenollari <anxhelocenollari@Anxhelos-MacBook-Pro.local>
2025-11-17 16:57:05 +0100

    Adding fonts

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,31 @@
-import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
+import { fontFamily } from "../constants/fonts";
 
 export default function RootLayout() {
-  return <Stack />;
+  const [loaded] = useFonts({
+    [fontFamily.light]: require("../../assets/fonts/Nunito-Light.ttf"),
+    [fontFamily.regular]: require("../../assets/fonts/Nunito-Regular.ttf"),
+    [fontFamily.medium]: require("../../assets/fonts/Nunito-Medium.ttf"),
+    [fontFamily.bold]: require("../../assets/fonts/Nunito-Bold.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+      }}
+      initialRouteName="(app)"
+    >
+      <Stack.Screen name="(app)" />
+      <Stack.Screen name="(auth)" />
+    </Stack>
+  );
 }
---
+++ src/constants/fonts.ts
@@ -0,0 +1,6 @@
+export const fontFamily = {
+  light: "NunitoLight",
+  regular: "NunitoRegular",
+  medium: "NunitoMedium",
+  bold: "NunitoBold",
+};
